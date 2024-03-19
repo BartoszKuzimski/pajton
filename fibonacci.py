7bb3292b8ba34c2ef26a69a6e3ca32bc99eaa08e
Bartosz Kuzimski <bartosz.kuzimski@atos.net>
2024-03-19 11:55:50 +0100

    fibo

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,8 @@
-a = 0
-b = 1
+fibo = [0,1]
 # F(X) = F(x-1) + F(x-2)
 
-for sum=0 in 10:
-    sum = (a-1) + (b-2)
+for i in range(8):
+    sum = fibo[-1] + fibo[-2]
+    fibo.append(sum)
 
-print(sum)
+print(fibo)
