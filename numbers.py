87a3d10bff26a223a2f65f21a7df91bdac9abc5b
Bartosz Kuzimski <bartosz.kuzimski@atos.net>
2024-03-19 15:14:30 +0100

    dziłania na plikach

---
+++ numbers.py
@@ -0,0 +1,33 @@
+x = 10
+y = -5
+sum = x + y
+print(f"Sum: {sum}")
+
+print("#####################################################################")
+
+a = 2.5
+b = 0.5
+product = a * b
+print(f"Product: {product}")
+
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+result = 10//3
+print(f"Integer Division Result: {result}")
+
+print("#####################################################################")
+
+float_result = 10.0//3
+print(f"Float from Integer Division: {float_result}")
+
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+
+float_value = 3.14
+int_value = int(float_value)
+print(f"Converted Integer: {int_value}")
+
+print("#####################################################################")
+
+int_number = 5
+float_number = float(int_number)
+print(f"Converted Float: {float_number}")
