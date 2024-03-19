57e1d91bb664a1873b722c16a55f731981612235
Bartosz Kuzimski <bartosz.kuzimski@atos.net>
2024-03-19 09:35:09 +0100

    listy

--- listy.py
+++ listy.py
@@ -1,2 +1,14 @@
 my_list = [1, 2, 3, "Python", 3.14]
 print(my_list)
+############
+############
+first_item = my_list[0] 
+print(first_item) # wydrukowanie pierwszego elementu
+############
+############
+my_list.append("new item")
+print(my_list) # wydrukowanie dodanego elementu  'new item'
+############
+############
+my_list.remove("Python")
+print(my_list) # wydrukowanie usunietego elementu  'Python'
